d1c8edc543a893ac71351cd6302bf006c08a81a0
Monika Januszek <monika.januszek.mail@gmail.com>
2024-04-02 16:40:41 +0200

    Remove the task from the array added

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const addInput = document.getElementById("next-task-input");
 const tasksList = document.getElementById("tasks-list");
 let inputValue = "";
 const myTasks = [
-  { description: "Buy concert tickets" },
-  { description: "Organize halloween party" },
-  { description: "Meet with Julia" },
+  { description: "Buy concert tickets", id: 1 },
+  { description: "Organize halloween party", id: 2 },
+  { description: "Meet with Julia", id: 3 },
 ];
 
 function displayTasksList() {
@@ -14,6 +14,8 @@ function displayTasksList() {
     let li = document.createElement("li");
     li.classList.add("task");
     li.textContent = task.description;
+    li.setAttribute("data-id", task.id);
+    li.addEventListener("click", removeFromListFn);
     tasksList.appendChild(li);
   });
 }
@@ -22,7 +24,7 @@ function addNewTaskFn() {
   if (inputValue.trim() === "") {
     return;
   } else {
-    myTasks.push({ description: inputValue });
+    myTasks.push({ description: inputValue, id: myTasks.length + 1 });
     inputValue = "";
     addInput.value = "";
     console.log(myTasks);
@@ -32,6 +34,15 @@ function addNewTaskFn() {
 
 function getInputChangeFn(e) {
   inputValue = e.target.value;
+}
+
+function removeFromListFn(e) {
+  const taskId = parseInt(e.target.getAttribute("data-id"));
+  const taskIndex = myTasks.findIndex((task) => task.id === taskId);
+  if (taskIndex !== -1) {
+    myTasks.splice(taskIndex, 1);
+    displayTasksList();
+  }
 }
 
 addInput.addEventListener("change", getInputChangeFn);
